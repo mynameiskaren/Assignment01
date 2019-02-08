a0e26b0df433c2bf7e648080e0719bb84f0994f1
Karen Castro <karen.castro@live.com>
2019-02-07 19:34:18 -0800

    Assignment01
    First Assignment of FEWD 2 class.

---
+++ js/script.js
@@ -0,0 +1,158 @@
+//STEP 1
+//var someMonth;
+//function theMonth(){};
+//var currentMonth, summerMonth, myLibraryFunction;
+
+
+
+//STEP 2
+//console.log("Numberic Literal: " + 3.1415 + 
+//            "\nString Literal: Hello, World!" + 
+//            "\nBoolean Literal: " + false + 
+//            "\nNull Literal: " + null);
+
+
+
+//STEP 3
+//var three = 3, two = 2, num;
+//num = two * three + 12 / 2;
+//three = "Three is: " + (Math.sqrt(81) / 3);
+//console.log("Num is: " + num + "\n" + three);
+
+
+
+//STEP 4
+//var strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, boolContactYou;
+
+
+
+//STEP 5
+//var strAddress, strCity, intYourAge, strReferralSource, boolContactYou;
+//
+//var strFirstName = "Karen", strLastName = "Castro";
+//
+//strAddress = "1234 Apple Street";
+//strCity = "San Diego";
+//strReferralSource = "From flier.";
+//
+//var strState = "CA";
+//var intZipCode = "92111";
+//
+//intYourAge = prompt("What is your age?");
+//boolContactYou = confirm("May we contact you?");
+
+
+
+//STEP 6
+//var coercedResult = 1 + " is the number one.";
+//console.log(coercedResult);
+
+
+
+//STEP 7
+//var first  = false + " means it's not " + true;
+//console.log(first);
+//
+//var second = 2 + true;
+//console.log(second);
+
+
+
+//STEP 8
+//// Not valid, below is the fixed version.
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//console.log(someString);
+
+
+
+//STEP 9
+//var whoKnows = null, unknown;
+//console.log(whoKnows + "\n" + unknown);
+
+
+
+//STEP 10
+//var whoKnows;
+//console.log(typeof "Bird" + "\n" + typeof 3.14 + "\n" + typeof false + "\n" + typeof ["one", "two", "three"] + "\n" + typeof whoKnows);
+
+
+//STEP 11
+//alert("Hello " + "Karen" + "! Welcome to the JavaScript Class!");
+
+
+//STEP 12
+//var name = "Karen";
+//alert("Hello" + name + ", welcome to the JavaScript Class!");
+
+
+//STEP 13
+//var name = "Karen";
+//var course = "JavaScript";
+//alert("Hello" + name + ", welcome to the " + course + " Class!");
+
+
+//STEP 14
+//var name = "Karen";
+//var course = "JavaScript";
+//alert("Hello" + name + ".\nWelcome to the " + course + " Class!");
+
+
+//STEP 15
+//var name = prompt("What is your name?");
+//var course = "JavaScript";
+//alert("Hello" + name + ".\nWelcome to the " + course + " Class!");
+
+
+//STEP 16
+//var name = prompt("What is your name?");
+//var course = prompt("Great!\nAnd what is the name of the course you are taking?");
+//alert("Hello" + name + ".\nWelcome to the " + course + " Class!");
+
+
+//STEP 17
+//var x = 10;
+//var y = 20;
+//console.log(x + y);
+
+
+//STEP 18
+//var x = 20;
+//x += 20;
+//console.log(x);
+
+
+//STEP 19
+//var x = 20;
+//x *= 5;
+//console.log(x);
+
+
+//STEP 20
+//var x = 20 % 3;
+//x /= 1;
+//console.log(x);
+
+
+//STEP 21
+//var answerTrue = 500 > 1;
+//console.log(answerTrue);
+
+
+//STEP 22
+//var answerFalse = !(9 > 1)
+//console.log(answerFalse);
+
+
+//STEP 23
+//var widget = new Object();
+//console.log(typeof widget);
+
+
+//STEP 24
+//var widget = new Object();
+//console.log(widget instanceof Object);
+
+
+//STEP 25
+//var widget = new Object();
+//console.log(widget instanceof Array);
